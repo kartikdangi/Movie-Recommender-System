9729667e740e9a23f37564df6b1280b4eb2c061f
Kartik <66125923+kartikdangi@users.noreply.github.com>
2022-06-25 22:47:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+import pickle
+import streamlit as st
+import requests
+import base64
+
+# Fetching posters using IMDB api
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+# Recommending a movie 
+def recommend(movie, movies):
+    ind = list(movies['title'].values()).index(movie)
+    distances = sorted(list(enumerate(similarity[ind])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies['movie_id'][i[0]]
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies['title'][i[0]])
+
+    return recommended_movie_names,recommended_movie_posters
+
+
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('movie_dict.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+print(type(similarity))
+movie_list = movies['title'].values()
+
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie, movies)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
+
+
+def sidebar_bg(side_bg):
+
+   side_bg_ext = 'gif'
+
+   st.markdown(
+      f"""
+      <style>
+      [data-testid="stSidebar"] > div:first-child {{
+          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+      }}
+      </style>
+      """,
+      unsafe_allow_html=True,
+      )
+
+side_bg = './src/g.gif'
+sidebar_bg(side_bg)
